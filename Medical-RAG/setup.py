209adfdc88fc6e2037628ad0f21e98244d7586d5
Chaitanya Sawant <csawant.cs7@gmail.com>
2024-12-24 21:13:38 +0530

    initial commit

---
+++ Medical-RAG/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical RAG Bot',
+    version= '0.0.0',
+    author= 'Chaitanya Sawant',
+    author_email= 'chaitanya.aiwork@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
